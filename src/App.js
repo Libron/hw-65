0c105d0e0c9887c21d3d9f8c7eebdb7fe8347579
Urmat Bekboev <bek.urmat@gmail.com>
2019-02-08 03:08:15 +0600

    Changed routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Layout from "./components/Layout/Layout";
+import {Route, Switch} from "react-router";
+import Page from "./containers/Page";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Layout>
+            <Switch>
+                <Route path="/" exact render={() => <h1>Hellow</h1>} />
+                <Route path="/pages/:name" exact component={Page} />
+                <Route render={() => <h1>Not FOund</h1>} />
+            </Switch>
+        </Layout>
     );
   }
 }
